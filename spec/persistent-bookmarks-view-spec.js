156bfba80796c9ae0770ea1468d0220c5296ccc8
John <john.wong@tradegecko.com>
2017-05-22 14:28:10 +0800

    Initial project setup

---
+++ spec/persistent-bookmarks-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PersistentBookmarksView from '../lib/persistent-bookmarks-view';
+
+describe('PersistentBookmarksView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
